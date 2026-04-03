8dbbf2011b084d587b53425ce84955cb87582308
Sergey <skanaev93@mail.ru>
2026-04-03 17:58:09 +0300

    add github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+const githubPagesBasePath = '/vis-timeline-monitoring/';
+
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? githubPagesBasePath : '/',
   plugins: [react()],
   server: {
     port: 5173,
@@ -12,4 +15,4 @@ export default defineConfig({
       },
     },
   },
-});
+}));
